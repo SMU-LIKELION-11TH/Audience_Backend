92733657393130911741b50ab35a89847d82bf5c
BaekSeungW00 <ph1losophe@naver.com>
2023-06-29 01:08:21 +0900

    백승우_models.py 수정

--- util/models.py
+++ util/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from account import Userable
-from job import Postable
+
 class Like(models.Model):
-    userable = models.ForeignKey("Userable", on_delete=models.CASCADE, null=True)
-    postable = models.ForeignKey("Postable", on_delete=models.CASCADE, null=True)
+    userable = models.ForeignKey('account.Userable', on_delete=models.CASCADE, null=True)
+    postable = models.ForeignKey("employ.Postable", on_delete=models.CASCADE, null=True)
 
 class Dislike(models.Model):
-    userable = models.ForeignKey("Userable", on_delete=models.CASCADE, null=True)
-    postable = models.ForeignKey("Postable", on_delete=models.CASCADE, null=True)
+    userable = models.ForeignKey("account.Userable", on_delete=models.CASCADE, null=True)
+    postable = models.ForeignKey("employ.Postable", on_delete=models.CASCADE, null=True)
 
 class Interest(models.Model):
     name = models.CharField(max_length=16, null=False, default='')
 
 class Hashtag(models.Model):
     name = models.CharField(max_length=10, null=False, default='')
-    postable = models.ManyToManyField(Postable)
+    postable = models.ManyToManyField('employ.Postable', related_name='posts')
