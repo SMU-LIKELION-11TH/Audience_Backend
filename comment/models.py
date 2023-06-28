6d64b6d2b70b0e2c2bef00ad246084d6d7322a33
seoyeseon <dptjs6476@gmail.com>
2023-06-29 00:39:24 +0900

    서예선 models.py

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # postable = models.ForeignKey('employ.Postable', on_delete=models.CASCADE)
+    # userable = models.ForeignKey('employ.Userable', on_delete=models.CASCADE)
+
+class Reply(models.Model):
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.ForeignKey('comment.Comment', on_delete=models.CASCADE)
+    # postable = models.ForeignKey('employ.Postable', on_delete=models.CASCADE)
+    # userable = models.ForeignKey('employ.Userable', on_delete=models.CASCADE)
+
