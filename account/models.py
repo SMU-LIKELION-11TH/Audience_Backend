03aedbc2093431f35696cde61f2e555ea6bee179
BaekSeungW00 <ph1losophe@naver.com>
2023-06-29 00:15:13 +0900

    백승우_models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from util import Interest
 
-# Create your models here.
+class Userable(AbstractUser):
+    first_name = None
+    last_name = None
+    email = None
+    username = models.CharField(max_length=16, unique=True, null=False, default='')
+    CHOICES = [
+        ('선택', None), ('구직자', 'Applicant'), ('구인자', 'Employer')
+    ]
+    type = models.CharField(max_length=3, choices=CHOICES)
+    interest = models.ManyToManyField(Interest)
+
+class Applicant(Userable):
+    nickname = models.CharField(max_length=16, unique=True, null=False, default='')
+    age = models.IntegerField(null=False)
+    GENDER = [
+        ('남자', 'male'), ('여자', 'female'), ('선택 안 함', None)
+    ]
+    gender = models.CharField(max_length=10, choices=GENDER)
+    school = models.CharField(max_length=30, null=False, default=None)
+
+class Employer(Userable):
+    company = models.CharField(max_length=20, null=False, default='')
+    phone_num = models.CharField(max_length=12, null=False, default='')
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
--- util/models.py
+++ util/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from account import Userable
+from job import Postable
+class Like(models.Model):
+    userable = models.ForeignKey("Userable", on_delete=models.CASCADE, null=True)
+    postable = models.ForeignKey("Postable", on_delete=models.CASCADE, null=True)
 
-# Create your models here.
+class Dislike(models.Model):
+    userable = models.ForeignKey("Userable", on_delete=models.CASCADE, null=True)
+    postable = models.ForeignKey("Postable", on_delete=models.CASCADE, null=True)
+
+class Interest(models.Model):
+    name = models.CharField(max_length=16, null=False, default='')
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=10, null=False, default='')
+    postable = models.ManyToManyField(Postable)
