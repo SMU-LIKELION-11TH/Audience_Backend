d65d6407d306fea31ee9ba2b43598d59d3f4671a
seoyeseon <dptjs6476@gmail.com>
2023-06-29 04:43:21 +0900

    models.py
     postable과 userable를 수정하고 추가적으로 postable에 조회수 모델을 추가하고 Employ_post에 경력/신입을 선택할 수 있는 모델을 만들었습니다. 그리고 이름이 충돌 나는 두 가지는 _ref를 붙여서 구분했습니다. 상속 관계와 foreign 관계를 구분해서 이해하고 수정했습니다. comment, config, employ 이 세 가지의 cpython가 자꾸 충돌 나지만 그냥 올렸습니다.

--- job/models.py
+++ job/models.py
@@ -1,16 +1,16 @@
 from django.db import models
+from employ.models import Postable
 
 # Create your models here.
-class Job_post(models.Model):
-    image = models.ImageField(upload_to='uploads/')
-    # postable = models.ForeignKey('employ.Postable', on_delete=models.CASCADE)
-    # userable = models.ForeignKey('employ.Userable', on_delete=models.CASCADE)
+class Job_post(Postable,models.Model):
+    image = models.ImageField(upload_to='post/job/')
+    # userable = models.ForeignKey('account.Userable', on_delete=models.CASCADE)
 
-class Freepost(models.Model):
-    # postable = models.ForeignKey('employ.Postable', on_delete=models.CASCADE)
+class Freepost(Postable,models.Model):
+    pass
 
 class report(models.Model):
     CONTENT_CHOICES = (('a','사기/도배'),('b','욕설/비하'))
     content = models.CharField(max_length=1, default='a', choices=CONTENT_CHOICES)
-    # postable = models.ForeignKey('employ.Postable', on_delete=models.CASCADE)
+    postable = models.ForeignKey('employ.Postable', on_delete=models.CASCADE)
 
