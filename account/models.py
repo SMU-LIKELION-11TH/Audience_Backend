d0dd14152162153c44ef5ffaca071fd7e2039d37
BaekSeungW00 <ph1losophe@naver.com>
2023-07-05 17:43:54 +0900

    model field 수정

--- account/models.py
+++ account/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import AbstractUser
 class Userable(AbstractUser):
     first_name = None
     last_name = None
+    email = models.EmailField(unique=True)
     username = models.CharField(max_length=16, unique=True, null=False, default='')
     CHOICES = [
         ('선택', None), ('구직자', 'Applicant'), ('구인자', 'Employer')
     ]
     type = models.CharField(max_length=3, choices=CHOICES)
-    interest = models.ManyToManyField('util.Interest', related_name='interests')
+    interest = models.ManyToManyField('util.Interest', related_name='interests', blank=True)
 
 class Applicant(Userable):
     nickname = models.CharField(max_length=16, unique=True, null=False, default='')
@@ -30,4 +31,7 @@ class Applicant(Userable):
 
 class Employer(Userable):
     company = models.CharField(max_length=20, null=False, default='')
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating_sum = models.IntegerField(null=True) #총점
+    post_num = models.IntegerField(null=True) #후기글 수
+    # 회사 평점은 총점/후기글 수로 표현
+
