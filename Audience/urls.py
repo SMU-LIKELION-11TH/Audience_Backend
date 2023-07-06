b7af61c402e9065686bd54dc6dd747a5d87879e4
BaekSeungW00 <ph1losophe@naver.com>
2023-07-07 01:04:43 +0900

    search url 수정

--- Audience/urls.py
+++ Audience/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('main/', views.main_view, name='main_view'),
+    path('search/page/', views.search_page, name='search_page'),
+    path('search/<category>/<board_type>/<post_type>/<search_type>/')
+    # category: 구인/구직, board_type: Free/(구인/구직), post_type: 구인 -> 관심 분야/신입/경력, search_type: 검색 조건
 ]
