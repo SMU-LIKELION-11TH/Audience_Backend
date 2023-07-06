6b2afb70b29ffd6aa853f1bd0593c315f42967aa
BaekSeungW00 <ph1losophe@naver.com>
2023-07-07 01:03:53 +0900

    검색 기능 구현

--- Audience/views.py
+++ Audience/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from account.models import Employer
 from employ.models import Postable, Employ_post
-from job.models import Job_post
+from util.models import Hashtag, Interest, UserInterest
+from job.models import Job_post, Freepost
+import json
+from django.db.models import Q
 
 # 메인화면
 def main_view(request):
@@ -12,31 +16,103 @@ def main_view(request):
     return render(request, '메인화면 템플릿', context)
 
 # 검색페이지(검색어 get parameter)
-# def search_page(request):
-#     keyword = request.POST.get('keyword')
-#
-#     board_type = request.GET.get('board_type')
-#     post_type = request.GET.get('post_type')
-#     employ_post_type = request.GET.get('employ_post_type')
-#     if board_type:
-#         if post_type:
-#             if employ_post_type:
-#                 posts = .objects.filter(title__icontains=keyword)
-
+# 뷰만 띄움
+def search_page(request, category):
+    return render(request, '검색 화면 템플릿')
 
 # 검색 결과
-# def search_posts(request):
-#     keyword = request.GET.get('keyword')
-#     board_type = request.GET.get('board_type')
+# 매개변수로 검색어랑 카테고리 pk로 받아서 검색
+# ajax
+# 구인 카테고리면 employ에서 구직이면 job에서 자유면 freepost
 
+def search_posts(request):
+    data = json.loads(request.body)
+    keyword = data['keyword']
+    category = data['category']
+    board_type = data['board_type']
+    post_type = data['post_type']
+    search_type = data['search_type']
 
+    if category == "구직":
+        if board_type == "구직":
+            if search_type == "제목":
+                posts = Job_post.objects.filter(title__incontain=keyword)
+            elif search_type == "내용":
+                posts = Job_post.objects.filter(content__incontain=keyword)
+            elif search_type == "제목+내용":
+                posts = Job_post.objects.filter(Q(title__incontain=keyword) | Q(content__incontain=keyword))
+            elif search_type == "해시태그":
+                hashtag = Hashtag.objects.filter(name=keyword)
+                posts = Job_post.objects.filter(hashtag=hashtag)
+        else:
+            if search_type == "제목":
+                posts = Freepost.objects.filter(title__incontain=keyword)
+            elif search_type == "내용":
+                posts = Freepost.objects.filter(content__incontain=keyword)
+            elif search_type == "제목+내용":
+                posts = Freepost.objects.filter(Q(title__incontain=keyword) | Q(content__incontain=keyword))
+            elif search_type == "해시태그":
+                hashtag = Hashtag.objects.filter(name=keyword)
+                posts = Freepost.objects.filter(hashtag=hashtag)
+    elif category == "구인":
+        if board_type == "구인":
+            if post_type == "관심 분야":
+                user = request.user
+                interest = UserInterest.objects.filter(userable=user)
+                employer = Employer.objects.filter(interest=interest)
 
-    #data
-        # all/구직/구인
-        # 구인글/자유소통, 구직자들/자유소통
+                if search_type == "제목":
+                    posts = Employ_post.objects.filter(title__incontain=keyword, userable=employer)
+                elif search_type == "내용":
+                    posts = Employ_post.objects.filter(content__incontain=keyword, userable=employer)
+                elif search_type == "제목+내용":
+                    posts = Employ_post.objects.filter(Q(title__incontain=keyword, userable=employer)
+                                                       | Q(content__incontain=keyword, userable=employer))
+                elif search_type == "해시태그":
+                    hashtag = Hashtag.objects.filter(name=keyword, userable=employer)
+                    posts = Employ_post.objects.filter(hashtag=hashtag)
+                elif search_type == "회사":
+                    posts = employer.postable.filter(company__incontain=keyword, userable=employer)
 
-        # 구인 -> 관심분야/신입/경력
-        # 구직 -> all
+            elif post_type == "경력":
+                if search_type == "제목":
+                    posts = Employ_post.objects.filter(title__incontain=keyword, career='a')
+                elif search_type == "내용":
+                    posts = Employ_post.objects.filter(content__incontain=keyword, career='a')
+                elif search_type == "제목+내용":
+                    posts = Employ_post.objects.filter(Q(title__incontain=keyword, career='a')
+                                                       | Q(content__incontain=keyword, career='a'))
+                elif search_type == "해시태그":
+                    hashtag = Hashtag.objects.filter(name=keyword)
+                    posts = Employ_post.objects.filter(hashtag=hashtag, career='a')
+                elif search_type == "회사":
+                    posts = Employ_post.objects.filter(company__incontain=keyword)
 
-        # 분류 -> all/제목/...
+            elif post_type == "신입":
+                if search_type == "제목":
+                    posts = Employ_post.objects.filter(title__incontain=keyword, career='b')
+                elif search_type == "내용":
+                    posts = Employ_post.objects.filter(content__incontain=keyword, career='b')
+                elif search_type == "제목+내용":
+                    posts = Employ_post.objects.filter(Q(title__incontain=keyword, career='b')
+                                                       | Q(content__incontain=keyword, career='b'))
+                elif search_type == "해시태그":
+                    hashtag = Hashtag.objects.filter(name=keyword)
+                    posts = Employ_post.objects.filter(hashtag=hashtag, career='b')
+                elif search_type == "회사":
+                    posts = Employ_post.objects.filter(company__incontain=keyword, career='b')
 
+        else:
+            if search_type == "제목":
+                posts = Freepost.objects.filter(title__incontain=keyword)
+            elif search_type == "내용":
+                posts = Freepost.objects.filter(content__incontain=keyword)
+            elif search_type == "제목+내용":
+                posts = Freepost.objects.filter(Q(title__incontain=keyword) | Q(content__incontain=keyword))
+            elif search_type == "해시태그":
+                hashtag = Hashtag.objects.filter(name=keyword)
+                posts = Freepost.objects.filter(hashtag=hashtag)
+            elif search_type == "회사":
+                posts = Freepost.objects.filter(company__incontain=keyword)
+
+    return JsonResponse({'posts': posts})
