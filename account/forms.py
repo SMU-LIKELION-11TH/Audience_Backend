45d12445f02b7350ad11995c49c963f78ced0b5c
BaekSeungW00 <ph1losophe@naver.com>
2023-07-08 03:15:11 +0900

    form update

--- account/forms.py
+++ account/forms.py
@@ -1,26 +1,51 @@
 from django import forms
 from account.models import Userable, Applicant, Employer
+from util.models import Interest
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class ApplicantCreateForm(UserCreationForm):
+    interests = forms.ModelMultipleChoiceField(
+        queryset=Interest.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
     class Meta(UserCreationForm.Meta):
         model = Applicant
         fields = ['email', 'username', 'password1', 'password2',
-                  'interest', 'nickname', 'age', 'gender',
+                  'interests', 'nickname', 'age', 'gender',
                   'school', 'career']
 
 class EmployerCreateForm(UserCreationForm):
+    interests = forms.ModelMultipleChoiceField(
+        queryset=Interest.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
     class Meta(UserCreationForm.Meta):
         model = Employer
         fields = ['email', 'username', 'password1', 'password2',
-                  'interest', 'company', 'rating_sum', 'post_num']
+                  'interests', 'company']
 
 class ApplicantUpdateForm(UserChangeForm):
+    interests = forms.ModelMultipleChoiceField(
+        queryset=Interest.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
     class Meta(UserChangeForm.Meta):
         model = Applicant
-        fields = ['nickname', 'age', 'gender', 'school', 'career']
+        fields = ['nickname', 'age', 'gender', 'school', 'career', 'interests']
 
 class EmployerUpdateForm(UserChangeForm):
+    interests = forms.ModelMultipleChoiceField(
+        queryset=Interest.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
     class Meta(UserChangeForm.Meta):
         model = Employer
-        fields = ['company']
+        fields = ['company', 'interests']
