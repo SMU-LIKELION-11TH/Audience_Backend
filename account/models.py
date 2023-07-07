83a0cd4f12e1108943854725f7cbcd045516213e
BaekSeungW00 <ph1losophe@naver.com>
2023-07-08 03:15:31 +0900

    Userable update

--- account/models.py
+++ account/models.py
@@ -5,12 +5,12 @@ class Userable(AbstractUser):
     first_name = None
     last_name = None
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=16, unique=True, null=False, default='')
+    username = models.CharField(max_length=16, unique=True, null=False, default='')     # 로그인 아이디
+    name = models.CharField(max_length=16, null=False, default='')
     CHOICES = [
         ('선택', None), ('구직자', 'Applicant'), ('구인자', 'Employer')
     ]
     type = models.CharField(max_length=3, choices=CHOICES)
-    interest = models.ManyToManyField('util.Interest', related_name='interests',blank = True, null =True)
 
 class Applicant(Userable):
     nickname = models.CharField(max_length=16, unique=True, null=False, default='')
@@ -31,7 +31,7 @@ class Applicant(Userable):
 
 class Employer(Userable):
     company = models.CharField(max_length=20, null=False, default='')
-    rating_sum = models.IntegerField(null=True) #총점
-    post_num = models.IntegerField(null=True) #후기글 수
-    # 회사 평점은 총점/후기글 수로 표현
+    # rating_sum = models.IntegerField(null=True) #총점
+    # post_num = models.IntegerField(null=True) #후기글 수
+    # # 회사 평점은 총점/후기글 수로 표현
 
