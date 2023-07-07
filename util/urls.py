024ae2ae28c0d63424eb14b9ac0e77b49eec8290
BaekSeungW00 <ph1losophe@naver.com>
2023-07-08 03:20:32 +0900

    delete no view functions

--- util/urls.py
+++ util/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('like/<int:post_id>/create/', views.add_like, name='add_like'),
     path('dislike/<int:post_id>/create/', views.add_dislike, name='add_dislike'),
-    path('rating/<int:post_id>/create/', views.add_rating, name='add_rating'),
-    path('hashtag/<int:post_id>/create/', views.add_hashtag, name='add_hashtag'),
-    path('interest/update/', views.update_interest, name='update_interest'),
 ]
