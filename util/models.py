72ce19bd42546ad30cb8200d40571c7d09a76ddd
BaekSeungW00 <ph1losophe@naver.com>
2023-07-08 17:32:21 +0900

    models.py update

--- util/models.py
+++ util/models.py
@@ -23,7 +23,7 @@ class Rating(models.Model):
     STARS = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
     rate = models.IntegerField(choices=STARS, null=False, default='0')
 
-class EmployerRating:
+class EmployerRating(models.Model):
     employer = models.ForeignKey("account.Employer", on_delete=models.CASCADE, null=True)
     applicant = models.ForeignKey("account.Applicant", on_delete=models.CASCADE, null=True)
     rating = models.ForeignKey("util.Rating", on_delete=models.CASCADE, null=True)
