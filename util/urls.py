015c05a988dd847c7b65bdcd68703e623addd060
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-07-10 04:30:14 +0900

    Update urls.py

--- util/urls.py
+++ util/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('like/<int:post_id>/create/', views.add_like, name='add_like'),
-    path('dislike/<int:post_id>/create/', views.add_dislike, name='add_dislike'),
+    path('like/create/', views.add_like, name='add_like'),
+    path('dislike/create/', views.add_dislike, name='add_dislike'),
 ]
